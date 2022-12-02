8f59e0c4f45d93fcb0439c8dc42bc94b54d2dca1
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-12-02 21:12:21 +0300

    ServerSocket

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws IOException {
+        ServerSocket serSocket = new ServerSocket(8000);
+        System.out.println("START SERVER");
+        serSocket.accept();
+        System.out.println("CLIENT CONNECTED");
     }
+
 }
