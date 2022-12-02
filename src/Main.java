bbd17ae619fff63c953d2f3dc3049d7238e0ecf4
Artyom Kalinkin <artyom.artyom.9494@mail.ru>
2022-12-02 21:36:02 +0300

    my first SERVER

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,24 @@
-import java.io.IOException;
+import java.io.*;
 import java.net.ServerSocket;
+import java.net.Socket;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        ServerSocket serSocket = new ServerSocket(8000);
-        System.out.println("START SERVER");
-        serSocket.accept();
-        System.out.println("CLIENT CONNECTED");
-    }
+            ServerSocket serSocket = new ServerSocket(8000);
+            System.out.println("START SERVER");
 
+            Socket socket = serSocket.accept();
+            System.out.println("CLIENT CONNECTED");
+
+        BufferedWriter writer = new BufferedWriter(
+                new OutputStreamWriter(
+                        socket.getOutputStream()));
+        writer.write("Hello from SERVER");
+        writer.newLine();
+        writer.flush();
+
+        writer.close();
+        socket.close();
+        serSocket.close();
+    }
 }
